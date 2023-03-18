920ca405885330de5a0807bfb132c6952c72df49
shorya1835 <shoryasinghal1835@gmail.com>
2023-03-18 14:32:54 +0530

    less go

--- faster_rcnn_implementation/config.py
+++ faster_rcnn_implementation/config.py
@@ -1,1 +1,32 @@
+import torch
 
+#Hyperparameters
+BATCH_SIZE=6
+RESIZE_TO=(256,224)
+NUM_EPOCHS=20
+NUM_WORKERS=0
+
+#Choosing between cpu or gpu
+DEVICE=torch.device(('cuda') if torch.cuda.is_available() else torch.device('cpu'))
+
+#Training images
+TRAIN_DIR='resized_data/train'
+
+#Validation images 
+VALID_DIR='resized_data/validation'
+
+#classes: 0 index is reserved for background
+CLASSES=[
+    '__background__','Aluminium foil', 'Battery', 'Blister pack', 'Bottle', 'Bottle cap', 'Broken glass', 'Can', 'Carton', 'Cup', 'Food waste', 'Glass jar', 'Lid', 'Other plastic', 'Paper', 'Paper bag', 'Plastic bag & wrapper', 'Plastic container', 'Plastic glooves', 'Plastic utensils', 'Pop tab', 'Rope & strings', 'Scrap metal', 'Shoe', 'Squeezable tube', 'Straw', 'Styrofoam piece', 'Unlabeled litter', 'Cigarette']
+
+NUM_CLASSES=len(CLASSES)
+
+# whether to visualize images after crearing the data loaders
+VISUALIZE_TRANSFORMED_IMAGES = True
+
+# location to save model and plots
+OUT_DIR = 'outputs'
+
+
+
+    
